20f5311d22f11eb473de0e530ab74f360c47fd61
antnf3 <antnf3@gmail.com>
2024-02-17 18:14:31 +0900

    7.6 Uploading Documents

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,29 +1,48 @@
 import streamlit as st
 import time
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
 
 st.set_page_config(page_title="DocumentGPT", page_icon="📑")
 
 st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
+
+file = st.file_uploader(
+    label="Upload a .txt .pdf or .docx file", type=["pdf", "txt", "docx"]
+)
 
 
-def send_message(message, role, save=True):
-    with st.chat_message(name=role):
-        st.write(message)
-        if save:
-            st.session_state["messages"].append({"message": message, "role": role})
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/{file.name}"
+    st.write(file_content, file_path)
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+        cache_dir = LocalFileStore(f"./.cache/embedings/{file.name}")
+        splitter = CharacterTextSplitter.from_tiktoken_encoder(
+            separator="\n",
+            chunk_size=600,
+            chunk_overlap=100,
+        )
+        loader = UnstructuredFileLoader(file_path)
+        docs = loader.load_and_split(text_splitter=splitter)
+        embeddings = OpenAIEmbeddings()
+        cached_embeddir = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+        vectorstore = FAISS.from_documents(docs, cached_embeddir)
+        retriever = vectorstore.as_retriever()
+    return retriever
 
 
-for msg in st.session_state["messages"]:
-    send_message(msg["message"], msg["role"], False)
-
-message = st.chat_input("Send a message")
-if message:
-    send_message(message=message, role="human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
-
-    with st.sidebar:
-        st.write(st.session_state["messages"])
+if file:
+    retriever = embed_file(file)
+    s = retriever.invoke("winston")
+    st.write(s)
