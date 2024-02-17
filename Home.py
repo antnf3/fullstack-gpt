43101b97712b0ce55059bde5bdc942afa40b3e2f
antnf3 <antnf3@gmail.com>
2024-02-17 17:40:42 +0900

    7.4 Chat Messages

--- Home.py
+++ Home.py
@@ -2,3 +2,17 @@ import streamlit as st
 
 st.set_page_config(page_title="Fullstack-gpt", page_icon="🔗")
 st.title("FullStack-GPT")
+
+st.markdown(
+    """
+# Hello!
+
+Welcome to my FullStack GPT Portfolio!
+
+Here are the apps I made:
+
+- [ ] [Document GPT](/DocumentGPT)
+- [ ] [Private GPT](/PrivateGPT)
+- [ ] [Quiz GPT](/QuizGPT)
+"""
+)
--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,29 @@
 import streamlit as st
+import time
+
+st.set_page_config(page_title="DocumentGPT", page_icon="📑")
 
 st.title("DocumentGPT")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(name=role):
+        st.write(message)
+        if save:
+            st.session_state["messages"].append({"message": message, "role": role})
+
+
+for msg in st.session_state["messages"]:
+    send_message(msg["message"], msg["role"], False)
+
+message = st.chat_input("Send a message")
+if message:
+    send_message(message=message, role="human")
+    time.sleep(2)
+    send_message(f"You said: {message}", "ai")
+
+    with st.sidebar:
+        st.write(st.session_state["messages"])
