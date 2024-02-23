9423edc58d5ee69b1a6b9ee599676068a3d053be
antnf3 <antnf3@gmail.com>
2024-02-23 21:28:54 +0900

    10.2 SitemapLoader

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,4 +1,4 @@
-from langchain.document_loaders import AsyncChromiumLoader
+from langchain.document_loaders import AsyncChromiumLoader, SitemapLoader
 from langchain.document_transformers import Html2TextTransformer
 import streamlit as st
 
@@ -16,6 +16,15 @@ Start by writing the URL of the website on the sidebars.
 """
 )
 
+
+@st.cache_data(show_spinner="loading website...")
+def load_website(url):
+    loader = SitemapLoader(url)
+    loader.requests_per_second = 1
+    docs = loader.load()
+    return docs
+
+
 with st.sidebar:
     url = st.text_input(
         "Write down a URL",
@@ -23,7 +32,14 @@ with st.sidebar:
     )
 
 if url:
-    loader = AsyncChromiumLoader([url])
-    docs = loader.load()
-    transformed = html2text_transformer.transform_documents(docs)
-    st.write(transformed)
+    # AsyncChromiumLoader
+    # loader = AsyncChromiumLoader([url])
+    # docs = loader.load()
+    # transformed = html2text_transformer.transform_documents(docs)
+    # st.write(transformed)
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("❌ Please write down a Sitemap URL.")
+    else:
+        docs = load_website(url)
+        st.write(docs)
