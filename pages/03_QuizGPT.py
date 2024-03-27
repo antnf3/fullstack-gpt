f118114bbf4365bde933f13c51b8d872d52790bc
antnf3 <antnf3@gmail.com>
2024-03-27 23:40:33 +0900

    9.6 Caching

---
+++ pages/03_QuizGPT.py
@@ -0,0 +1,285 @@
+import json
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.retrievers import WikipediaRetriever
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.schema import BaseOutputParser
+
+
+# 결과를 받아서 파이썬에서 사용할 객체로 변환한다.
+class JsonOutputParser(BaseOutputParser):
+    def parse(self, text: str):
+        text = text.replace("```", "").replace("json", "")
+        return json.loads(text)
+
+
+output_parser = JsonOutputParser()
+
+st.set_page_config(page_title="QuizGPT", page_icon="❓")
+
+st.title("QuizGPT")
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    model="gpt-3.5-turbo-0125",
+    streaming=True,
+    callbacks=[
+        StreamingStdOutCallbackHandler(),
+    ],
+)
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a helpful assistant that is role playing as a teacher.
+
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+
+    Use (o) to signal the correct answer.
+
+    Question examples:
+
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+    Your turn!
+
+    Context: {context}
+""",
+        ),
+    ]
+)
+questios_chain = {"context": format_docs} | questions_prompt | llm
+
+# AI에게 JSON파일 형식으로 질문을 가공해달다고 요청하기위한 Prompt
+# ```json 을 사용하면 일관되게 형식화할수있음. LLM이 다시 말을 걸지않고(물론이지요, 기꺼이 도와줄께요) 작성된 양식으로 응답만 준다.
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a powerful formatting algorithm.
+     
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+     
+    Example Input:
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+    
+     
+    Example Output:
+     
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }},
+                ]
+            }},
+                        {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }},
+                ]
+            }},
+                        {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }},
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }},
+                ]
+            }}
+        ]
+     }}
+    ```
+    Your turn!
+    Questions: {context}
+""",
+        )
+    ]
+)
+# {{ }} => langchain이 이 부분은 format하지 않기 위한용도
+
+formatting_chain = formatting_prompt | llm
+
+
+@st.cache_resource(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/quiz_gpt/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+        # splitter 선언
+        splitter = CharacterTextSplitter.from_tiktoken_encoder(
+            separator="\n",
+            chunk_size=600,
+            chunk_overlap=100,
+        )
+        # load file & split
+        loader = UnstructuredFileLoader(file_path)
+        docs = loader.load_and_split(text_splitter=splitter)
+        return docs
+
+
+# cache_data 는 아래 함수의 파라미터로 들어오는 값응 hash처리한다(데이터의 '서명' 생성)
+# 두번째 호출될때 hash값의 변경이 없으면 함수가 실행되지 않고 cache된 값을 반환한다.
+# 파라미터앞에 _를 붙이면 해당 파라미터는 hash처리를 하지 않는다.(변경유무와 상관없이 항상 cache된 결과를 리턴한다.)
+# 그래서 두번째 파라미터를 전달하여 hash처리 할수 있게한다.
+# hash할수 없는 매개변수가 있거나, streamlit이 데이터의 서명을 만들 수 없는 경우
+# 두번째가 변경되면 함수 재실행
+
+
+@st.cache_data(show_spinner="Making quiz...")
+def run_quiz_chain(_docs, topic):
+    chain = {"context": questios_chain} | formatting_chain | output_parser
+    return chain.invoke(docs)
+
+
+@st.cache_data(show_spinner="Searching Wikipedia")
+def wiki_search(term):
+    retriever = WikipediaRetriever(top_k_results=2)
+    docs = retriever.get_relevant_documents(term)
+    return docs
+
+
+with st.sidebar:
+    docs = None
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx, .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+        if file:
+            docs = split_file(file)
+    else:
+        topic = st.text_input("Name of the article")
+        if topic:
+            docs = wiki_search(topic)
+
+if not docs:
+    st.markdown(
+        """
+    Welcome to QuizGPT.
+
+    I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
+
+    Get started by uploading a file or searching on Wikipedia in the sidebar.
+    """
+    )
+else:
+
+    start = st.button("Generate Quiz")
+    if start:
+        # case 1)
+        # questions_response = questios_chain.invoke(docs)
+        # formatting_response = formatting_chain.invoke(
+        #     {"context": questions_response.content}
+        # )
+
+        # case2)
+        response = run_quiz_chain(docs, topic if topic else file.name)
+        st.write(response)
